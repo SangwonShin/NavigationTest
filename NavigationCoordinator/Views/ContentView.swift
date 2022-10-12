65a42741e85435c739c4e3814b30e1829ea7778c
신상원 <sinsang-won@sinsang-won-ui-iMac.local>
2022-10-12 14:09:12 +0900

    Navigation Test - 5

--- NavigationCoordinator/Views/ContentView.swift
+++ NavigationCoordinator/Views/ContentView.swift
@@ -8,58 +8,6 @@
 import Combine
 import SwiftUI
 
-final class NavigationHelper: ObservableObject {
-  @Published var view1To: Bool = false
-  @Published var view2To: Bool = false
-  @Published var view3To: Bool = false
-  @Published var view4To: Bool = false
-  
-  private var cancellables = Set<AnyCancellable>()
-  
-  init() {
-    bind()
-  }
-  
-  func popTo(viewID: ViewID) {
-    
-  }
-  
-  func popRoot() {
-    
-  }
-  
-  // TODO: - 상위 depth로 pop하는 경우, 처리
-  // FIXME: - 아래와 같은 구조에서 만약 navigaiton Depth가 100d이면 10초가 걸리는 문제
-  func bind() {
-    $view1To
-      .dropFirst()
-      .filter { $0 == false }
-      .delay(for: 0.1, scheduler: DispatchQueue.global())
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] _ in
-        self?.view2To = false
-      }.store(in: &cancellables)
-    
-    $view2To
-      .dropFirst()
-      .filter { $0 == false }
-      .delay(for: 0.1, scheduler: DispatchQueue.global())
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] _ in
-        self?.view3To = false
-      }.store(in: &cancellables)
-    
-    $view3To
-      .dropFirst()
-      .filter { $0 == false }
-      .delay(for: 0.1, scheduler: DispatchQueue.global())
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] _ in
-        self?.view4To = false
-      }.store(in: &cancellables)
-  }
-}
-
 struct ContentView: View {
   @EnvironmentObject
   var navigationHelper: NavigationHelper
@@ -67,11 +15,10 @@ struct ContentView: View {
   var body: some View {
     NavigationView {
       NavigationLink(
-        destination: NavigationLazyView(ContentView2()),
-        isActive: $navigationHelper.view1To
-      ) {
-        Text("Hello, World!")
-      }
+        isActive: $navigationHelper["1"].state ,
+        destination: { NavigationLazyView(ContentView2()) },
+        label: { Text("Hello, World!") }
+      )
       .isDetailLink(false)
       .navigationBarTitle("Root")
     }
@@ -84,11 +31,10 @@ struct ContentView2: View {
   
   var body: some View {
     NavigationLink(
-      destination: NavigationLazyView(ContentView3()),
-      isActive: $navigationHelper.view2To
-    ) {
-      Text("Hello, World #2!")
-    }
+      isActive: $navigationHelper["2"].state ,
+      destination: { NavigationLazyView(ContentView3()) },
+      label: { Text("Hello, World #2!") }
+    )
     .isDetailLink(false)
     .navigationBarTitle("Two")
   }
@@ -101,25 +47,20 @@ struct ContentView3: View {
   var body: some View {
     VStack {
       NavigationLink(
-        destination: NavigationLazyView(ContentView4()),
-        isActive: $navigationHelper.view3To
-      ) {
-        Text("Hello, World #3!")
-      }
+        isActive: $navigationHelper["3"].state ,
+        destination: { NavigationLazyView(ContentView4()) },
+        label: { Text("Hello, World #3!") }
+      )
       .isDetailLink(false)
       
       Button(
-        action: {
-          navigationHelper.view1To = false
-        }
+        action: { navigationHelper.popRoot() }
       ){
         Text("Pop to root")
       }
       
       Button(
-        action: {
-          navigationHelper.view2To = false
-        }
+        action: { navigationHelper.pop(viewID: "3", 1) }
       ){
         Text("Pop to prev")
       }
@@ -137,17 +78,13 @@ struct ContentView4: View {
       Text("Hello, World #4!")
       
       Button(
-        action: {
-          navigationHelper.view1To = false
-        }
+        action: { navigationHelper.popRoot() }
       ){
         Text("Pop to root")
       }
       
       Button(
-        action: {
-          navigationHelper.view2To = false
-        }
+        action: { navigationHelper.pop(viewID: "4", 2) }
       ){
         Text("Pop to 2")
       }
